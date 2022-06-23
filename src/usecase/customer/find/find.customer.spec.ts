28f9a8a6e8e2a4a13d58c5029e8c1250d79823fc
Robson S Rodrigues <robsoncraftsman@gmail.com>
2022-06-23 16:29:38 +0000

    Adicionado caso de uso para buscar um cliente

---
+++ src/usecase/customer/find/find.customer.spec.ts
@@ -0,0 +1,55 @@
+import { Sequelize } from "sequelize-typescript";
+import Customer from "../../../domain/customer/entity/customer";
+import Address from "../../../domain/customer/value-object/address";
+import CustomerModel from "../../../infrastructure/customer/repository/sequelize/customer.model";
+import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
+import FindCustomerUseCase from "./find.customer.usecase";
+
+describe('Find Customer Use Case', () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+
+        await sequelize.addModels([CustomerModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it('should find a customer', async () => {
+        const customer = new Customer("1", "João")
+        const address = new Address("Rua das Flores", 123, "11.1111-111", "São Paulo")
+        customer.changeAddress(address)
+
+        const customerRepository = new CustomerRepository()
+        await customerRepository.create(customer)
+
+        const input = {
+            id: "1"
+        }
+
+        const expectedOutput = {
+            id: "1",
+            name: "João",
+            address: {
+                street: "Rua das Flores",
+                city: "São Paulo",
+                number: 123,
+                zip: "11.1111-111"
+            }
+        }
+
+        const usecase = new FindCustomerUseCase(customerRepository)
+        const output = await usecase.execute(input)
+
+        expect(output).toEqual(expectedOutput)
+    })
+})
---
+++ src/usecase/customer/find/find.customer.usecase.ts
@@ -0,0 +1,22 @@
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
+import { InputFindCustomerDTO, OutputFindCustomerDTO } from "./find.customer.dto";
+
+export default class FindCustomerUseCase {
+    constructor(private customerRepository: CustomerRepositoryInterface) {
+
+    }
+
+    async execute(input: InputFindCustomerDTO): Promise<OutputFindCustomerDTO> {
+        const customer = await this.customerRepository.find(input.id);
+        return {
+            id: customer.id,
+            name: customer.name,
+            address: {
+                street: customer.Address.street,
+                city: customer.Address.city,
+                number: customer.Address.number,
+                zip: customer.Address.zip
+            }
+        }
+    }
+}
